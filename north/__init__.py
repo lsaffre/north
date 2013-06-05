d7c68ebe7a262b971d4218f46cd5a422c9f13122
Luc Saffre <luc.saffre@gmx.net>
2013-06-05 10:22:25 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0605.rst

--- north/__init__.py
+++ north/__init__.py
@@ -15,6 +15,7 @@ execfile(os.path.join(os.path.dirname(__file__),'setup_info.py'))
 
 __version__ = SETUP_INFO['version'] # 
 
+intersphinx_url = "http://north.lino-framework.org"
 
 #~ __author__ = "Luc Saffre <luc.saffre@gmx.net>"
 
